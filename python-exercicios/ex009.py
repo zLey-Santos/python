2bcd62a2f4044f8a1e9680c2984d5ec2d6c6c91d
zLey-Santos <wy.santos92@gmail.com>
2024-03-06 19:08:08 +0000

    Exibindo um Valor X da Tabuada

---
+++ python-exercicios/ex009.py
@@ -0,0 +1,13 @@
+tb = int(input('Digite um número para ver sua tabuada: '))
+print('_-_' * 4)
+print(f'{tb:2} x {1:2} = {tb * 1} \n '
+      f'{tb} x {2:2} = {tb * 2} \n '
+      f'{tb} x {3:2} = {tb * 3} \n '
+      f'{tb} x {4:2} = {tb * 4} \n '
+      f'{tb} x {5:2} = {tb * 5} \n '
+      f'{tb} x {6:2} = {tb * 6} \n '
+      f'{tb} x {7:2} = {tb * 7} \n '
+      f'{tb} x {8:2} = {tb * 8} \n '
+      f'{tb} x {9:2} = {tb * 9} \n '
+      f'{tb} x {10} = {tb * 10} ')
+print('-_-' * 4)
