853f938d8ebd0aed84e8c6d78c6d8d35215c4fe9
zLey-Santos <wy.santos92@gmail.com>
2024-03-09 20:09:17 +0000

    módulos

--- main-python/módulos/meu_emoji.py
+++ main-python/módulos/meu_emoji.py
@@ -1,2 +1,3 @@
 import emoji
-print(emoji.emojize('Olá, Mundo! :world_map:'))
+print(emoji.emojize('Olá, Mundo! :globe_showing_Americas:'))
+print(emoji.emojize('Olá, mundo! :beaming_face_with_smiling_eyes:'))
--- main-python/módulos/modulo01.py
+++ main-python/módulos/modulo01.py
@@ -1,5 +1,6 @@
-from math import sqrt, floor
+from math import sqrt, floor, ceil
 
 num = float(input('Digite um número: '))
 up = sqrt(num)
-print(f'O valor de {num:.0f} aredondado para cima é {floor(up)}')
+print(f'O valor de {num:.0f} aredondado para cima é {ceil(up)}')
+print(f'O valor de {num:.0f} aredondado para baixo é {floor(up)}')
