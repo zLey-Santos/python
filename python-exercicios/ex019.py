a5d1ea09dc9bfd1f4946c9ade7e446ef75f95568
zLey-Santos <wy.santos92@gmail.com>
2024-03-10 01:09:00 +0000

    Random Choice

--- python-exercicios/ex019.py
+++ python-exercicios/ex019.py
@@ -1,9 +1,8 @@
-import random
+from random import choice
 n1 = str(input('Primeiro Aluno: '))
 n2 = str(input('Primeiro Aluno: '))
 n3 = str(input('Primeiro Aluno: '))
 n4 = str(input('Primeiro Aluno: '))
 lista = [n1, n2, n3, n4]
-escolha = input('Pressione ENTER para escolher um nome aleatorio):')
-nome_aluno = random.choice(lista)
+nome_aluno = choice(lista)
 print(f'O aluno escolhido é: {nome_aluno}')
