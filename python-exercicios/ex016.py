8da52503f83a67f989d4dacd89e9260a71844b81
zLey-Santos <wy.santos92@gmail.com>
2024-03-10 00:46:11 +0000

    módulo math floor, trunc e int

--- python-exercicios/ex016.py
+++ python-exercicios/ex016.py
@@ -1,3 +1,4 @@
-from math import floor
+from math import floor, trunc
 inteiro = float(input('Digite um número float: '))
 print(f'O número {inteiro} tem a parte inteira {floor(inteiro)}')
+print(f'O número {inteiro} tem a parte inteira {trunc(inteiro)}')
