f1f7ce1653e4ea270a13517849b61351377440bb
zLey-Santos <wy.santos92@gmail.com>
2024-03-10 01:13:06 +0000

    Random shuffle

--- python-exercicios/ex020.py
+++ python-exercicios/ex020.py
@@ -1,7 +1,12 @@
-import random
-nomes = ['Ana', 'Pepe', 'Bruna', 'zLey']
+from random import shuffle
+
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+nomes = [n1, n2, n3, n4]
 input('Pressione ENTER para ver a ordem de apresentação dos trabalhos: ')
-random.shuffle(nomes)
+shuffle(nomes)
 print(f'A ordem de apresentação dos trabalhos são: {nomes}')
 
 # O método shuffle() pertence ao módulo random em Python é usado para embaralhar os elementos de uma sequência de forma aleatória.
