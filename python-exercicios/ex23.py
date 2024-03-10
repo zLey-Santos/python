f46232685a2176cab4104a33424017b4178fd795
zLey-Santos <wy.santos92@gmail.com>
2024-03-10 22:17:41 +0000

    Seprando dígitos de um número

---
+++ python-exercicios/ex23.py
@@ -0,0 +1,10 @@
+num = int(input('Informe um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f'Analisando o numero {num} \n'
+      f'Unidade: {u} \n'
+      f'Dezena:  {d} \n'
+      f'Centena: {c} \n'
+      f'Milhar:  {m}')
