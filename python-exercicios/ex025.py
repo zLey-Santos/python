45063f65f675d4f957056b545995472ae04b4830
zLey-Santos <wy.santos92@gmail.com>
2024-03-11 18:52:10 +0000

    Procurando uma String Usando in

---
+++ python-exercicios/ex025.py
@@ -0,0 +1,5 @@
+nome = str(input('Qual o seu nome completo? ')).strip()
+sv = nome == 'SILVA'
+print(f'Seu nome contem silva {'silva' in nome.lower()}')
+
+# in Verifica se um elemento está presente numa lista:
