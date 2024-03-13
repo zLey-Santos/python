28e52c6f8077107510752eaaf41a3921ba0c8764
zLey-Santos <wy.santos92@gmail.com>
2024-03-13 19:50:18 +0000

    Guess Game

--- python-exercicios/ex028.py
+++ python-exercicios/ex028.py
@@ -3,8 +3,8 @@ import random
 print('Tente adivinhar o número entre 1 e 5: ')
 
 num_cpu = random.randint(1, 5)
-
 num_user = int(input('O número escolhido foi: '))
+
 if num_user > 5:
     print('Por favor, insira um número válido. ')
 elif num_user == num_cpu:
