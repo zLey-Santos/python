dd0201ca68cee0bacba5bd9dab35075a98713821
zLey-Santos <wy.santos92@gmail.com>
2024-03-13 22:48:39 +0000

    Distancia em Km cClc ulâdPoculando  culando Preço

--- python-exercicios/ex030.py
+++ python-exercicios/ex030.py
@@ -1,8 +1,14 @@
-num = int(input('Digite um número: '))
+km = float(input('Qual a distancia da sua viagem em Km: '))
 
-par_impar = num % 2
+km_limit = 200
+valor_kmp = 0.50
+dis_p = km * valor_kmp
 
-if par_impar == 0:
-    print('Par')
-else:
-    print('Ímpar')
+valor_kml = 0.45
+dis_l = km * valor_kml
+
+if km <= km_limit:
+    print(f'O valor para uma viagem de {km:.0f}Km R${dis_p:.2f}')
+elif km > km_limit:
+    print(f'O valor para uma viagem de {km:.0f}Km R${dis_l}')
+
