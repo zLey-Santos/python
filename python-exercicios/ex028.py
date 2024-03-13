8f689cbc5f277b427abeb1ac3d5adc858636c835
zLey-Santos <wy.santos92@gmail.com>
2024-03-13 23:08:37 +0000

    Jogo de Adivinhação V-1.0

--- python-exercicios/ex028.py
+++ python-exercicios/ex028.py
@@ -1,13 +1,17 @@
-import random
+from random import randint
+from time import sleep # Biblioteca de timeout
 
-print('Tente adivinhar o número entre 1 e 5: ')
+print('-=-' * 20)
+print('Vou pensar em um número entre (0 e 5). Tente adivinhar... ')
+print('-=-' * 20)
 
-num_cpu = random.randint(1, 5)
+num_cpu = randint(0, 5)
 num_user = int(input('O número escolhido foi: '))
-
+print('ANALISANDO...')
+sleep(2)
 if num_user > 5:
     print('Por favor, insira um número válido. ')
 elif num_user == num_cpu:
     print(f'Parabéns! Você adivinhou o número corretamente {num_cpu}. ')
 else:
-    print(f'Tente outra vez. O número correto era {num_cpu}')
+    print(f'Tente outra vez. O número correto era {num_cpu} e não {num_user}.')
