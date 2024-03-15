2685d9b8ad6381769e36557847da4cdc64f8114c
zLey-Santos <wy.santos92@gmail.com>
2024-03-15 10:15:44 +0000

    Par Ímpar

--- python-exercicios/ex030.py
+++ python-exercicios/ex030.py
@@ -3,6 +3,6 @@ num = int(input('Digite um número: '))
 par_impar = num % 2
 
 if par_impar == 0:
-    print('Par')
+    print('\033[1:32m Par \033[m')
 else:
-    print('Ímpar')
+    print('\033[1:31m Ímpar\033[m')
