60fec1dffb7bc0f74bed3804c05f3f69206a8265
zLey-Santos <wy.santos92@gmail.com>
2024-03-17 16:59:34 +0000

    calculando porcentagem v.2

--- python-exercicios/ex034.py
+++ python-exercicios/ex034.py
@@ -5,7 +5,7 @@ salario = float(input('Digite o salário atual do funcionário: '))
 # 'Limpa' é usado para limpar a formatação de cores, 'sal' e 'R$' são usados para colorir o texto.
 cores = {
     'limpa': '\033[m',
-    'sal': '\033[1:31m',  # Vermelho brilhante para destacar o valor do salário
+    'salario': '\033[1:31m',  # Vermelho brilhante para destacar o valor do salário
     'R$': '\033[1:32m'  # Verde brilhante para destacar a moeda R$
 }
 
@@ -15,12 +15,12 @@ if salario > 5000:
     reajuste_10 = (salario * 10 / 100) + salario
     # Exibe o novo salário com reajuste de 10% destacando a moeda e o valor em vermelho.
     print(f'O seu salário passa a ser de '
-          f'{cores["R$"]}R${cores["sal"]}{reajuste_10:.2f}{cores["limpa"]},'
+          f'{cores["R$"]}R${cores["salario"]}{reajuste_10:.2f}{cores["limpa"]},'
           f' 10% de reajuste!')
 else:
     # Calcula o reajuste de 15% no salário.
     reajuste_15 = (salario * 15 / 100) + salario
     # Exibe o novo salário com reajuste de 15% destacando a moeda e o valor em vermelho.
     print(f'O seu salário passa a ser de '
-          f'{cores["R$"]}R${cores["sal"]}{reajuste_15:.2f}{cores["limpa"]},'
+          f'{cores["R$"]}R${cores["salario"]}{reajuste_15:.2f}{cores["limpa"]},'
           f' 15% de reajuste')
