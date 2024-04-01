19f8389683a3c19e97b83dcb273bf3fa2faaa702
zLey-Santos <wy.santos92@gmail.com>
2024-04-01 14:33:37 +0100

    Números Primos (FOR-IF)

--- python-exercicios/ex052.py
+++ python-exercicios/ex052.py
@@ -9,7 +9,7 @@ for c in range(1, num + 1):
   print(f'{c}\033[m', end = ' ')
 print(f'\n(O número \033[32m{num}\033[m foi divisível por \033[36m{tot}\033[m vezes)')
 if tot == 2:
-  print(f'\033[1;32m O número \033[33m{num}\033[m \033[32m É primo\033[m')
+  print(f'\033[32m O número \033[33m{num}\033[m \033[32m É primo\033[m')
 else:
-  print(f'\033[1;31m O Número \033[36m{num}\033[36m \033[1;31m Não é primo\033[m')
+  print(f'\033[31m O Número \033[36m{num}\033[36m \033[31m Não é primo\033[m')
   
