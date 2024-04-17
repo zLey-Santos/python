e1fc249c03064e4c7b2b21de73e61ece53f6d484
zLey-Santos <wy.santos92@gmail.com>
2024-04-17 16:39:49 +0100

    Sequência de de Fibonacci v1.0 MELHORADA

--- python-exercicios/ex063.py
+++ python-exercicios/ex063.py
@@ -1,17 +1,30 @@
-print(f'{'--' * 15}')
+# Imprime uma linha de separação para o título
+print('-' * 30)
+# Imprime o título da sequência de Fibonacci
 print('Sequência de Fibonacci')
-print(f'{'--' * 15}')
+# Imprime outra linha de separação após o título
+print('-' * 30)
 
-n = int(input('Quantos termos voê quer mostrar? '))
-t1 = 0
-t2 = 1
-print(f'{t1} -> {t2}', end=' ') 
+# Solicita ao usuário o número de termos da sequência a serem mostrados
+n = int(input('Quantos termos você quer mostrar? '))
 
+# Inicializa os dois primeiros termos da sequência de Fibonacci
+t1, t2 = 0, 1
+# Imprime os dois primeiros termos na mesma linha
+print(f'{t1} -> {t2}', end=' ')
+
+# Inicializa o contador para acompanhar o número de termos impressos
 cont = 3
+# Executa um loop para calcular e imprimir os próximos termos da sequência
 while cont <= n:
-  t3 = t1 + t2
-  print(f'{t3}', end=' -> ')
-  t1 = t2
-  t2 = t3
-  cont += 1
-print('FIM')  
+    # Calcula o próximo termo da sequência como a soma dos dois últimos termos
+    t3 = t1 + t2
+    # Imprime o próximo termo com uma seta (->) antes
+    print(f'-> {t3}', end=' ')
+    # Atualiza os dois últimos termos para o próximo ciclo do loop
+    t1, t2 = t2, t3
+    # Incrementa o contador para acompanhar o progresso
+    cont += 1
+
+# Após imprimir todos os termos solicitados, imprime "FIM" para indicar o término da sequência
+print('FIM')
